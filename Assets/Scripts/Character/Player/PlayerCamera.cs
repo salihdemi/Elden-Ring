8966e38070142bb36a368239084ec43a0ad1248b
salihdemi <81927519+salihdemi@users.noreply.github.com>
2025-07-06 18:41:17 +0300

    x

--- Assets/Scripts/Character/Player/PlayerCamera.cs
+++ Assets/Scripts/Character/Player/PlayerCamera.cs
@@ -7,6 +7,13 @@ public class PlayerCamera : MonoBehaviour
     public static PlayerCamera instance;
 
     public Camera cameraObject;
+
+    public PlayerManager player;
+
+
+    [Header("Camera Settings")]
+    private Vector3 cameraVelocity;
+    private float cameraSmoothSpeed = 1;
     private void Awake()
     {
         if(instance == null)
@@ -22,5 +29,18 @@ public class PlayerCamera : MonoBehaviour
     {
         cameraObject = Camera.main;
         DontDestroyOnLoad(gameObject);
+    }
+
+    public void HandleAllCameraActions()
+    {
+        if(player != null)
+        {
+
+        }
+    }
+
+    private void FollowTarget()
+    {
+        Vector3 targetCameraPosition = Vector3.SmoothDamp(transform.position, player.transform.position, ref cameraVelocity, cameraSmoothSpeed * Time.deltaTime);
     }
 }
